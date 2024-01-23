a3ffb5c54ce47b5c50ae17c0aa23f126f4bd73b5
garchomp-game <otoka.japanesedrum@gmail.com>
2024-01-23 14:58:21 +0900

    commit

--- test.js
+++ test.js
@@ -1,20 +1,5 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
-// 自分の得意な言語で
-// Let's チャレンジ！！
-
-function customLoop(value, kaigyou) {
-  for(let i = 1; i <= value; i++) {
-    process.stdout.write(String(i));
-    if(i != value) {
-      process.stdout.write(" ")
-    }
-    if(kaigyou && i == value) {
-      process.stdout.write("\n")
-    }
-  }
-}
-
 var lines = [];
 var reader = require('readline').createInterface({
   input: process.stdin,
@@ -24,11 +9,6 @@ reader.on('line', (line) => {
   lines.push(line);
 });
 reader.on('close', () => {
-  let count = lines[0];
-  let values = lines[1].split(" ");
-  let checkLast = true;
-  for(let i = 1; i <= count; i ++) {
-    if(i == count) checkLast = false
-    customLoop(values[i - 1], checkLast);
-  }
+  let values = lines[0].split(' ').map(Number);
+  console.log(values[0].toFixed(values[1]));
 });
