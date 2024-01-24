1243d426ebbe116cc21c37e7956df4ca3c87e1c3
garchomp-game <otoka.japanesedrum@gmail.com>
2024-01-24 15:07:46 +0900

    commit

--- test.js
+++ test.js
@@ -1,14 +1,24 @@
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-var lines = [];
-var reader = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-reader.on('line', (line) => {
-  lines.push(line);
-});
-reader.on('close', () => {
-  let values = lines[0].split(' ').map(Number);
-  console.log(values[0].toFixed(values[1]));
-});
+// 九九の表を作る
+// スペース二つとバーティカルライン( | )で、数字は二桁で統一
+
+for(let i = 1; i <= 9; i++) {
+  for(let j = 1; j <= 9; j++) {
+    let value = String(i * j);
+    if(j !== 9) {
+      process.stdout.write(`${value.padStart(2)} | `);
+    } else {
+      process.stdout.write(`${value.padStart(2)}`);
+    }
+    // 最終行でなければ=で区切る
+  }
+  if(i !== 9) {
+    process.stdout.write("\n");
+    let num = (2 * 9) + (3 * 8);
+    let str = "";
+    for(let k = 0; k < num; k++) {
+      str += "=";
+    }
+    process.stdout.write(str);
+    process.stdout.write("\n");
+  }
+}
