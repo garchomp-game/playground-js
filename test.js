b33cea7b1b197228d649110513266998b7d8390f
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-25 23:18:36 +0900

    commit

--- test.js
+++ test.js
@@ -1,38 +1,24 @@
 process.stdin.resume();
-process.stdin.setEncoding("utf8");
+process.stdin.setEncoding('utf8');
 // 自分の得意な言語で
 // Let's チャレンジ！！
 var lines = [];
-var reader = require("readline").createInterface({
+var reader = require('readline').createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
-reader.on("line", (line) => {
+reader.on('line', (line) => {
   lines.push(line);
 });
-
-reader.on("close", () => {
-  let aGroupCount = Number(lines[0]);
-  let aGroup = [];
-  for (let i = 1; i <= aGroupCount; i++) {
-    aGroup.push(Number(lines[i]));
-  }
-  let bGroupCount = Number(lines[aGroupCount + 1]);
-  let bGroup = [];
-  for (let i = aGroupCount + 2; i <= aGroupCount + bGroupCount + 1; i++) {
-    bGroup.push(Number(lines[i]));
-  }
-  let changeGroup = "A";
-  for (let i = 1; i <= 31; i++) {
-    if (aGroup.includes(i) && bGroup.includes(i)) {
-      console.log(changeGroup);
-      changeGroup == "A" ? (changeGroup = "B") : (changeGroup = "A");
-    } else if (aGroup.includes(i)) {
-      console.log("A");
-    } else if (bGroup.includes(i)) {
-      console.log("B");
+reader.on('close', () => {
+  let [A, B, N] = lines[0].split(' ').map(Number);
+  let numbers = lines[1].split(' ').map(Number);
+  
+  for (let i = 0; i < N; i++) {
+    if (numbers[i] == A) {
+      console.log(B);
     } else {
-      console.log("x");
+      console.log(numbers[i]);
     }
   }
 });
